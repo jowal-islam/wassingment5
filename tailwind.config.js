dab683f8f48e638614fd0c2e90b157934987258a
jowal-islam <ranabrindabon@gmail.com>
2025-08-27 22:27:47 +0600

    main section layout done

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./hotline.html", "./src/**/*.{js,ts,jsx,tsx}"], // ফাইল path গুলো ঠিক করে নিও
+  theme: {
+    extend: {
+      fontFamily: {
+        hind: ['"Hind Madurai"', "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
